32bec7f1120068ad888e4d2c3b4e9890a2d826bf
Tim Parsons <timmparsons85@gmail.com>
2020-08-14 11:11:13 -0700

    Updates

---
+++ precache-manifest.d7e6a277ec2ea34ef4991fcfc20e169f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f3536266eaebeb653d9df8bbc47a839",
+    "url": "/newsSite/index.html"
+  },
+  {
+    "revision": "ee6ab63a596d19a0f61e",
+    "url": "/newsSite/static/css/main.3a4a8290.chunk.css"
+  },
+  {
+    "revision": "069b72ac183e614fda38",
+    "url": "/newsSite/static/js/2.3ddda952.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/newsSite/static/js/2.3ddda952.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee6ab63a596d19a0f61e",
+    "url": "/newsSite/static/js/main.147765c0.chunk.js"
+  },
+  {
+    "revision": "d30294318b9a23610a81",
+    "url": "/newsSite/static/js/runtime-main.7b8c691a.js"
+  }
+]);
